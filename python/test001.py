243006e6092e48c4e72a785f750e6a4baf58e2c2
Nick Fox-Gieg <nick@fox-gieg.com>
2017-12-18 12:09:27 -0500

    python

--- python/test001.py
+++ python/test001.py
@@ -7,8 +7,12 @@ def main():
     parser.add_argument('files', type=str, nargs='+', help="Files to process")
     args = parser.parse_args()
 
-    for filename in args.files:
-        print(os.listdir("."))
+    for filepath in args.files:
+        targetList = ""
+        targets = os.listdir(str(filepath))
+        for target in targets:
+            targetList += filepath + "/" + target + " "
+        os.system("./../src/image-stitching " + targetList)
 
 if __name__ == '__main__':
     main()
